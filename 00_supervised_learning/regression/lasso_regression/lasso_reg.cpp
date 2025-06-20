2bdfa27025a8790bcdda432aa6033556082cdee3
Jan Schlegel <jschlegel@student.ethz.ch>
2025-06-20 23:03:14 +0200

    fixed standardization

--- 00_supervised_learning/regression/lasso_regression/lasso_reg.cpp
+++ 00_supervised_learning/regression/lasso_regression/lasso_reg.cpp
@@ -1,16 +1,17 @@
 #include "lasso_reg.hpp"
-#include <Eigen/Dense>
-#include <iostream>
-#include <cmath>
 
+#include <Eigen/Dense>
+#include <cmath>
+#include <iostream>
 
 /**
  * @brief Fit the linear regression model to the data
- * 
+ *
  * @param X Feature matrix
  * @param y Target vector
  */
-void LassoReg::fit(const Eigen::MatrixXd& X, const Eigen::VectorXd& y) {
+void LassoReg::fit(const Eigen::MatrixXd& X, const Eigen::VectorXd& y)
+{
     std::cout << "Fitting Lasso regression model..." << std::endl;
     int n = X.cols();
     int m = X.rows();
@@ -19,83 +20,103 @@ void LassoReg::fit(const Eigen::MatrixXd& X, const Eigen::VectorXd& y) {
     // Copy of the coefficients to check for convergence:
     Eigen::VectorXd old_coefficients = coefficients;
 
-
     // Standardize X:
-    Eigen::VectorXd X_mean = X.colwise().mean();
-    Eigen::VectorXd std = ((X.rowwise() - X_mean.transpose()).array().square().colwise().sum() / (m - 1)).sqrt();
+    X_mean = X.colwise().mean();
+    X_std =
+        ((X.rowwise() - X_mean.transpose()).array().square().colwise().sum() /
+         (m - 1))
+            .sqrt();
+    y_mean = y.mean();
+    y_std = sqrt((y.array() - y_mean).square().sum() / (m - 1));
     // Add 1e-12 to avoid division by zero
-    Eigen::MatrixXd X_standardized = (X.rowwise() - X_mean.transpose()).array().rowwise() / (std.transpose().array() + 1e-12);
+    Eigen::MatrixXd X_standardized =
+        (X.rowwise() - X_mean.transpose()).array().rowwise() /
+        (X_std.transpose().array() + 1e-12);
+    Eigen::VectorXd y_standardized =
+        (y.array() - y_mean) / (y_std + 1e-12);
 
-    for (int iter = 0; iter < max_iter; ++iter) {
+    for (int iter = 0; iter < max_iter; ++iter)
+    {
         // Copy of the old coefficients to check for convergence:
         old_coefficients = coefficients;
         std::cout << "Iteration: " << iter << "/" << max_iter << std::endl;
         // Coordinate descent: iterate over coefficients
-        for (int j = 0; j < n; ++j) {
+        for (int j = 0; j < n; ++j)
+        {
             coefficients[j] = 0.0;
-            // Calculate partial residuals
-            Eigen::VectorXd residuals = y - X_standardized * coefficients;
+            
+            //Calculate partial residuals
+            Eigen::VectorXd residuals = y_standardized -
+                                        X_standardized * coefficients;
+            //Calculate correlation between j-th feature and residuals
             double rho = X_standardized.col(j).dot(residuals);
-            // Update the intercept of the regression model (not regularized)
-            if (j == 0){
-                coefficients[j] = rho / (X_standardized.col(j).squaredNorm() + 1e-12);
-            }
-            else{
-                // Update j'th coefficient with soft-thresholding
-                coefficients[j] = soft_thresholding(rho, lambda) / (X_standardized.col(j).squaredNorm() + 1e-12);
-            }
+
+            //Apply soft thresholding
+            coefficients[j] = soft_thresholding(rho, lambda) /
+                              (X_standardized.col(j).squaredNorm() + 1e-12);
         }
 
         // Check for convergence
-        if ((coefficients - old_coefficients).norm() < tol) {
-            std::cout << "Converged after " << iter << " iterations" << std::endl;
+        if ((coefficients - old_coefficients).norm() < tol)
+        {
+            std::cout << "Converged after " << iter << " iterations"
+                      << std::endl;
             break;
         }
     }
+    //Scale coefficients back to original scale:
+    // beta_original[j] = beta_standardized[j] * (y_std / X_std[j]);
+    for (int j = 0; j < n; ++j)
+    {
+        coefficients[j] *= (y_std / (X_std[j] + 1e-12));
+    }
+    // Calculate intercept on original scale:
+    intercept = y_mean;
+    for (int j = 0; j < n; ++j) {
+        intercept -= X_mean[j] * coefficients[j];
+    }
 }
-
 
 /**
  * @brief Predict the target values for new data points
- * 
+ *
  * @param X Feature matrix
  * @return Eigen::VectorXd Predicted target values
  */
-Eigen::VectorXd LassoReg::predict(const Eigen::MatrixXd& X) {
-    int m = X.rows();
-    //standardize X:
-    Eigen::VectorXd mean = X.colwise().mean();
-    Eigen::VectorXd std = ((X.rowwise() - mean.transpose()).array().square().colwise().sum() / (m - 1)).sqrt();
-    //add 1e-12 to avoid division by zero
-    Eigen::MatrixXd X_standardized = (X.rowwise() - mean.transpose()).array().rowwise() / (std.transpose().array() + 1e-12);
-
-    return X_standardized * coefficients;
+Eigen::VectorXd LassoReg::predict(const Eigen::MatrixXd& X)
+{
+    return X * coefficients + Eigen::VectorXd::Constant(X.rows(), intercept);
 }
-
 
 /**
  * @brief Get the coefficients of the linear regression model
- * 
+ *
  * @return Eigen::VectorXd Coefficients of the linear regression model
  */
-Eigen::VectorXd LassoReg::get_coefficients() const {
+Eigen::VectorXd LassoReg::get_coefficients() const
+{
     return coefficients;
 }
 
-
 /**
  * @brief Soft thresholding operator
- * 
+ *
  * @param rho Input value
  * @param lambda Regularization parameter
  * @return double Soft thresholded value
  */
-double LassoReg::soft_thresholding(double rho, double lambda) {
-    if (rho < -lambda) {
+double LassoReg::soft_thresholding(double rho, double lambda)
+{
+    if (rho < -lambda)
+    {
         return rho + lambda;
-    } else if (rho > lambda) {
+    }
+    else if (rho > lambda)
+    {
         return rho - lambda;
-    } else {
+    }
+    else
+    {
         return 0;
     }
 }
--- 00_supervised_learning/regression/lasso_regression/lasso_reg.hpp
+++ 00_supervised_learning/regression/lasso_regression/lasso_reg.hpp
@@ -1,31 +1,28 @@
 #ifndef LassoReg_H
 #define LassoReg_H
 
-#include <iostream>
 #include <Eigen/Dense>
-
+#include <iostream>
 
 /**
  * @brief LassoReg class for lasso regression
- * 
+ *
  */
-class LassoReg {    
-public:
-    LassoReg(
-        double lambda = 1.0, 
-        int max_iter = 1000,
-        double tol = 1e-6
-    ): lambda(lambda), max_iter(max_iter), tol(tol){};
+class LassoReg
+{
+   public:
+    LassoReg(double lambda = 1.0, int max_iter = 1000, double tol = 1e-6)
+        : lambda(lambda), max_iter(max_iter), tol(tol){};
     /**
      * @brief Fit the linear regression model to the data
-     * 
+     *
      * @param X Feature matrix
      * @param y Target vector
      */
     void fit(const Eigen::MatrixXd& X, const Eigen::VectorXd& y);
     /**
      * @brief Predict the target values for new data points
-     * 
+     *
      * @param X Feature matrix
      * @return Eigen::VectorXd  Predicted target values
      */
@@ -33,18 +30,19 @@ public:
 
     /**
      * @brief Get the coefficients of the linear regression model
-     * 
+     *
      * @return Eigen::VectorXd Coefficients of the linear regression model
      */
     Eigen::VectorXd get_coefficients() const;
 
-private:
+   private:
     // Coefficients of the linear regression model
     Eigen::VectorXd coefficients;
     double intercept;
     Eigen::VectorXd X_mean;
     Eigen::VectorXd X_std;
     double y_mean;
+    double y_std;
     // Regularization parameter
     double lambda;
     // Maximum number of iterations for coordinate descent
@@ -52,16 +50,14 @@ private:
     // Tolerance for stopping criterion
     double tol;
 
-
     /**
      * @brief Soft thresholding operator
-     * 
+     *
      * @param rho Input value
      * @param lambda Regularization parameter
      * @return double Soft thresholded value
      */
     double soft_thresholding(double rho, double lambda);
-
 };
 
-#endif //LassoReg_H
+#endif  // LassoReg_H
